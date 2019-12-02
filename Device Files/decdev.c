a0a460d0272ac4f26a2ce8cfaa0e365fa1b36e9e
kashinkoji <chandan18386@iiitd.ac.in>
2019-12-02 12:49:29 +0530

    decryption skeleton added

---
+++ Device Files/decdev.c
@@ -0,0 +1,69 @@
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <uaccess.h>
+#include <string.h>
+
+#define DEVICE_NAME "decdev"
+
+static struct file_operations dev_operations = {
+    .open = dev_open,
+    .release = dev_release
+    .read = dev_read,
+    .write = dev_write,
+};
+
+static int dev_open(struct inode *, struct file *);
+static int dev_release(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static ssize_t dev_write(struct file *, char *, size_t, loff_t *);
+
+int reg;
+
+static char buffer[256] = {};
+
+int init_module(void) {
+
+    reg = register_chrdev(0,DEVICE_NAME,dev_operations);
+
+    if(reg < 0)
+        printk("decdev ERROR : Device not getting registered\n");
+
+    else
+        printk("decdev SUCCESS : Device successfully registered\n");
+
+}
+
+void cleanup_void(void) {
+
+    unregister_dev(reg,DEVICE_NAME);
+    printk("decdev SUCCESS : Device successfully unregistered\n");
+
+}
+
+
+static int dev_open(struct inode *ino, struct file *fil) {
+
+    printk("decdev OPEN : Device successfully opened\n");
+    return 0;
+}
+
+static int dev_release(struct inode *ino, struct file *fil) {
+
+    printk("decdev RELEASE : Device successfully released\n");
+    return 0;
+}
+
+static ssize_t dev_read(struct file *fil, char *data, ssize_t data_len, loff_t *t) {
+    int i = copy_to_user(data, buffer, sizeof(*buffer));
+    if(i == 0)
+        printk("decdev READ : data read successfully from the device\n");
+    return 0;
+}
+
+static ssize_t dev_write(struct file *fil, char *data, ssize_t data_len, loff_t *t) {
+    printk("decdev WRITE : data written successfully to the device\n");
+    return 0;
+}
+
+
+
--- Device Files/encdev.c
+++ Device Files/encdev.c
@@ -1,4 +1,7 @@
 #include <linux/fs.h>
+#include <linux/module.h>
+#include <uaccess.h>
+#include <string.h>
 
 #define DEVICE_NAME "encdev"
 
@@ -16,42 +19,50 @@ static ssize_t dev_write(struct file *, char *, size_t, loff_t *);
 
 int reg;
 
+static char buffer[256] = {};
+
 int init_module(void) {
 
     reg = register_chrdev(0,DEVICE_NAME,dev_operations);
 
     if(reg < 0)
-        printk("ERROR : Device not getting registered\n");
+        printk("encdev ERROR : Device not getting registered\n");
 
     else
-        printk("SUCCESS : Device successfully registered\n");
+        printk("encdev SUCCESS : Device successfully registered\n");
 
 }
 
 void cleanup_void(void) {
 
     unregister_dev(reg,DEVICE_NAME);
-    printk("SUCCESS : Device successfully unregistered\n");
+    printk("encdev SUCCESS : Device successfully unregistered\n");
 
 }
 
 
 static int dev_open(struct inode *ino, struct file *fil) {
 
+    printk("decdev OPEN : Device successfully opened\n");
     return 0;
 }
 
 static int dev_release(struct inode *ino, struct file *fil) {
 
+    printk("encdev RELEASE : Device successfully released\n");
     return 0;
 }
 
 static ssize_t dev_read(struct file *fil, char *data, ssize_t data_len, loff_t *t) {
-
+    int i = copy_to_user(data, buffer, sizeof(*buffer));
+    if(i == 0)
+        printk("encdev READ : data read successfully from the device\n");
+    return 0;
 }
 
 static ssize_t dev_write(struct file *fil, char *data, ssize_t data_len, loff_t *t) {
-
+    printk("encdev WRITE : data written successfully to the device\n");
+    return 0;
 }
 
 
